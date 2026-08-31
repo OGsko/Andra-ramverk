32156f7fcb388da4d47b4f8200a1d7004019f748
oskar <oskarl1@live.com>
2026-08-31 19:03:46 +0200

    Button added and signal to show of solidJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import { createSignal } from 'solid-js'
+
 
 function App() {
+const [count, setCount] = createSignal(0)
+
+
   return (
     <>
       <h1>Hello World!</h1>
+
+      <p>{count()}</p>
+
+      <button onClick={() => setCount(count() + 1)}>+1</button>
+
     </>
   )
 }
